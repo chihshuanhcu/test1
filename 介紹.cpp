519a35b43f92d3f58a44b1f388dd62d0855d715f
chihshuanhcu <chihshuanchu@yahoo.com.tw>
2016-10-06 15:18:13 +0800

    as

---
+++ 介紹.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+	
+	cout<<"我的名字叫:";
+	cout<<"朱芷萱";
+	cout<<endl;
+	cout<<endl;
+	
+	
+	cout<<"我的班級是:"; 
+	cout<<"資工2A";
+	cout<<endl;
+	cout<<endl;
+	
+	cout<<"我的學號是:";
+	cout<<"A4213052";
+	
+	
+	
+}
